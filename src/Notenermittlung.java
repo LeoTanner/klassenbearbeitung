61d90ef4d39965a788f0958bc4ff5d1c242085c5
LeoTanner <s50991@edu.campus02.at>
2021-10-15 19:11:31 +0200

    second commit - added comments

--- src/Notenermittlung.java
+++ src/Notenermittlung.java
@@ -4,15 +4,20 @@ public class Notenermittlung
 {
     public static void main(String[] args)
     {
+
+        //Scanner initialisieren und Input für die Punkteanzahl
         Scanner scan = new Scanner(System.in);
         System.out.println("Geben sie ihre Punkteanzahl an: ");
         int Testpunkte = scan.nextInt();
+        //Output -> Ausführung der Notenbestimmung
         System.out.println(Notenbestimmung(Testpunkte));
     }
 
     public static String Notenbestimmung (int Punkte)
     {
+        //Punkte werden beim Aufrufen der Methode mitgenommen
         int Note;
+        //Überprüfung, in was für einem Bereich die Punkte liegen
         if(Punkte<=50)
         {
             Note = 5;
