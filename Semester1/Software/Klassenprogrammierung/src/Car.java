a24e9dc711e7f35c881d995c45c80ecbdafc2d51
LeoTanner <s50991@edu.campus02.at>
2021-10-16 13:42:03 +0200

    git commit - Konto als Wiederholung für Methoden und OOP

--- Semester1/Software/Klassenprogrammierung/src/Car.java
+++ Semester1/Software/Klassenprogrammierung/src/Car.java
@@ -7,4 +7,99 @@ public class Car
     public int horsepower;
     public double co2Emission;
     public int currentspeed;
+
+
+    public void initialize (String manufacturer, String colour, int productionYear, String model, int horsepower, double co2Emission, int currentspeed)
+    {
+        this.manufacturer = manufacturer;
+        this.colour = colour;
+        this.productionYear = productionYear;
+        this.model = model;
+        this.horsepower = horsepower;
+        this.co2Emission = co2Emission;
+        this.currentspeed = currentspeed;
+    }
+
+    public void printcar()
+    {
+        System.out.println(getManufacturer() + " "+ getModel() + " "+ getColour());
+    }
+
+
+    public void setManufacturer(String manufacturer)
+    {
+        //Parameter / lokale Variablen in Methoden, wie Attribute verwenden
+        if(manufacturer.equalsIgnoreCase("FIAT"))
+        {
+            this.manufacturer = manufacturer;
+        }
+        else
+        {
+            System.out.println("Falscher Hersteller");
+        }
+
+    }
+
+    public void accelerate()
+    {
+        currentspeed += 10;
+    }
+
+    public void accelerate (int speed)
+    {
+        currentspeed += speed;
+    }
+
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public int getProductionYear() {
+        return productionYear;
+    }
+
+    public void setProductionYear(int productionYear) {
+        this.productionYear = productionYear;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getHorsepower() {
+        return horsepower;
+    }
+
+    public void setHorsepower(int horsepower) {
+        this.horsepower = horsepower;
+    }
+
+    public double getCo2Emission() {
+        return co2Emission;
+    }
+
+    public void setCo2Emission(double co2Emission) {
+        this.co2Emission = co2Emission;
+    }
+
+    public int getCurrentspeed() {
+        return currentspeed;
+    }
+
+    public void setCurrentspeed(int currentspeed) {
+        this.currentspeed = currentspeed;
+    }
 }
--- Semester1/Software/Klassenprogrammierung/src/CarDemo.java
+++ Semester1/Software/Klassenprogrammierung/src/CarDemo.java
@@ -6,22 +6,21 @@ public class CarDemo
         Car golf5_G_876TU = new Car(); //Object von Klasse Car erstellt
         Car FiatPanda_G_123RU = new Car();
         Car golf5_LB_345LX = new Car();
+        Car VwLupo_GU_123AB = new Car();
 
-        golf5_G_876TU.manufacturer = "Volkswagen";
-        golf5_G_876TU.model = "Golf V";
-        golf5_G_876TU.horsepower = 90;
-        golf5_G_876TU.colour = "black";
-        golf5_G_876TU.co2Emission = 300;
+        golf5_G_876TU.setManufacturer("Volkswagen");
+        golf5_G_876TU.setModel("Golf V");
+        golf5_G_876TU.setHorsepower(90);
+        golf5_G_876TU.setColour("black");
+        golf5_G_876TU.setCo2Emission(300);
 
-        FiatPanda_G_123RU.manufacturer = "Fiat";
-        FiatPanda_G_123RU.model = "Panda";
-        FiatPanda_G_123RU.horsepower = 75;
-        FiatPanda_G_123RU.colour = "red";
-        FiatPanda_G_123RU.co2Emission = 120;
+        FiatPanda_G_123RU.setManufacturer("Fiat");
+        FiatPanda_G_123RU.setModel("V");
+        FiatPanda_G_123RU.setHorsepower(75);
+        FiatPanda_G_123RU.setColour("red");
+        FiatPanda_G_123RU.setCo2Emission(150);
 
-        golf5_G_876TU.colour = "black";
-        golf5_LB_345LX.model = "Golf V";
-
-        System.out.println("Golf 5 aus Graz: "+golf5_G_876TU.colour);
+        VwLupo_GU_123AB.initialize("VW","yellow",1994,"Lupo",45,100,40);
+        VwLupo_GU_123AB.printcar();
     }
 }
