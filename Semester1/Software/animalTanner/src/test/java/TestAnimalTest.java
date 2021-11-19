d73836c11c42d286cfafbca354cdabaac6ae2edf
LeoTanner <s50991@edu.campus02.at>
2021-11-19 23:56:32 +0100

    git commit -
    Animal filled with setter, getter, constructor and methods
    DemoAnimal filled with test cases and tried methods
    TestAnimalTest tried maven unit tests

---
+++ Semester1/Software/animalTanner/src/test/java/TestAnimalTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestAnimalTest {
+
+   int a;
+   boolean b = true;
+
+   @org.junit.jupiter.api.BeforeEach
+
+   //in der pom rechtclick dependency junit jupiter 5.8.1
+   //dann plugin setup maven surefire suchen,
+      // org darüber eingeben und version 2.2.0 normal
+
+
+   void setUp() {
+      a = 1;
+      b = false;
+   }
+
+   @Test
+   void myFirstTest() {
+      assertTrue(a > 0);
+   }
+
+   @Test
+   void mySecondTest()
+   {
+      assertTrue(b == false);
+   }
+}
